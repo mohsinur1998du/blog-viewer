00140657399483834e6e7bd6173f5755275a0ffd
Md Mohsinur Rahman <mohsinur1999@gmail.com>
2025-01-24 09:05:40 +0600

    added profile page

---
+++ app/profile/page.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+export default async function page() {
+  const { isAuthenticated } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (!isUserAuthenticated) {
+    redirect("/api/auth/login");
+  }
+  if (!isUserAuthenticated) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full text-center">
+          <h1 className="text-3xl font-semibold text-gray-800 mb-4">
+            Welcome to your profile!
+          </h1>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full text-center">
+        <h1 className="text-3xl font-semibold text-gray-800 mb-4">
+          Welcome to your profile!
+        </h1>
+      </div>
+    </div>
+  );
+}
